f16af25773adea4dde4f75f31a59b626ab533798
Oeverlandian <leonardo.maessen@telenet.be>
2024-08-18 21:52:30 +0200

    First version, you can connect to a server and talk to other people.

---
+++ server.py
@@ -0,0 +1,37 @@
+from flask import Flask
+from flask_socketio import SocketIO, send
+from datetime import datetime
+import socket
+
+server_name = None
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@socketio.on('message')
+def handle_message(data):
+    username = data['username']
+    message = data['message']
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    
+    full_message = f"{username} | {timestamp}\n{message}"
+    print(f"Received message from {username} at {timestamp}: {message}")
+    
+    send(full_message, broadcast=True)
+
+def get_local_ip():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.settimeout(0)
+        s.connect(('8.8.8.8', 80))
+        local_ip = s.getsockname()[0]
+    except Exception as e:
+        local_ip = 'Unable to determine local IP'
+    finally:
+        s.close()
+    return local_ip
+
+if __name__ == '__main__':
+    local_ip = get_local_ip()
+    print(f"Server is running on: {local_ip}:5000")
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
